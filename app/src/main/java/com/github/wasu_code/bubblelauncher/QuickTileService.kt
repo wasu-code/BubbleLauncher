418afa4ba7f5f8c8d0f61dd0e38cf7dd77820485
wasu-code <61418403+wasu-code@users.noreply.github.com>
2025-09-27 17:20:47 +0200

    feat: collapse QS Tile after launching bubble chooser

--- app/src/main/java/com/github/wasu_code/bubblelauncher/QuickTileService.kt
+++ app/src/main/java/com/github/wasu_code/bubblelauncher/QuickTileService.kt
@@ -1,8 +1,11 @@
 package com.github.wasu_code.bubblelauncher
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.service.quicksettings.TileService
 import android.content.Intent
+import android.os.Build
 import androidx.annotation.RequiresPermission
 import com.github.wasu_code.bubblelauncher.util.BubbleHelper
 
@@ -11,10 +14,21 @@ class QuickTileService : TileService() {
     override fun onClick() {
         super.onClick()
 
-        // Create intent for BubbleChooserActivity and post bubble
-        BubbleHelper.createBubbleChannel(this)
-        val intent = Intent(this, BubbleChooserActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        BubbleHelper.postBubble(this, intent, null)
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            startActivityAndCollapse(pendingIntent)  // API 34+
+        } else {
+            @SuppressLint("StartActivityAndCollapseDeprecated")
+            startActivityAndCollapse(intent)  // API 24–33
+        }
     }
 }
